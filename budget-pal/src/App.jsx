d9391ece03cffd60f11ca110e5aa5c25527dd6d9
briifernandez <bfernandez@ucsd.edu>
2023-09-20 00:36:24 -0700

    1-5

---
+++ budget-pal/src/App.jsx
@@ -0,0 +1,57 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+//Libary 
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+//layouts
+import Main, { mainLoader } from "./layouts/Main"
+
+//actions
+import { logoutAction } from "./actions/logout";
+//routes
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+    loader: mainLoader,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        element: <Dashboard/>,
+        loader: dashboardLoader,
+        action: dashboardAction,
+        errorElement: <Error/>
+    
+      },
+      {
+        path: "logout",
+        action: logoutAction
+      },
+   
+
+    ]
+
+  },
+
+  
+
+])
+
+function App() {
+  return <div className="App">
+    <RouterProvider router={router} />
+    <ToastContainer/>
+  
+  </div>
+}
+
+export default App
+ 
---
+++ budget-pal/src/components/Nav.jsx
@@ -0,0 +1,45 @@
+//rrd imports
+import { Form, NavLink } from "react-router-dom"
+//assets
+import moneytree from "../assets/money-tree.png"
+//libary
+import { TrashIcon } from '@heroicons/react/24/solid'
+
+
+const Nav = ({ userName }) => {
+    return (
+        <nav>
+            <NavLink
+                to="/"
+                aria-label="Go to home"
+
+            >
+                <img src={moneytree} alt="" height={40} />
+                <span>BudgetPal</span>
+            </NavLink>
+            {
+                userName && (
+                    <Form
+                        method="post"
+                        action="/logout"
+                        onSubmit={(event) => {
+                            if(!confirm("Delete user and all data?")) 
+                            {
+                                event.preventDefault()
+                            }
+                        }}
+                    >
+                        <button type="submit" className="btn btn--warning">
+                            <span>Delete User</span>
+                            <TrashIcon width={20}/>
+
+                        </button>
+
+
+                    </Form>
+                )
+            }
+        </nav>
+    )
+}
+export default Nav
